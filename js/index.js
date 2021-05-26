e5edb084a96f42d5db684a924413b4c761c911e3
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2021-05-26 18:25:22 -0300

    reinicio

--- js/index.js
+++ js/index.js
@@ -1,10 +1,33 @@
-let range  = document.querySelector('input') 
-let value = document.getElementById('value')
+let range  = document.querySelector('input')
+let pageviews = document.getElementById('pageviews')
+let price = document.getElementById('value')
+const allprices= [
+    {
+        pageviews: '10K',
+        price: 8
+    },
+    {
+        pageviews: '50K',
+        price: 12
+    },
+    {
+        pageviews: '100K',
+        price:16
+    },
+    {
+        pageviews: '500K',
+        price: 24
+    },
+    {
+        pageviews: '1M',
+        price: 36
+    }
+]
+pageviews.innerHTML = `${price.pageviews}`
+let multiplier = yearlyBilling ? 0.75 : 1
+let finalPrice = allPrices[sv].price * multiplier
+  htmlPrice.innerHTML = `$${finalPrice}.00`
+
 range.addEventListener('input',function(){
   console.log(range.textContent = this.value)
 })
-switch (range){
-    case 1:
-        
-    break;
-}
