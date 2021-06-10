0f1d5629956afb3cc23203010b8163713eccede5
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2021-06-10 13:41:45 -0300

    n

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 let range  = document.querySelector('input')
 let pageviews = document.getElementById('pageviews')
 let price = document.getElementById('value')
+let biling = document.getElementsByClassName('billing')
+let dot = document.getElementsByClassName('dot')
+let yearlyBilling = false
+biling.addEventListener('click',function(e){
+    if (dot.style.float == "right") {
+        dot.style.float = "left"
+        toggle.classList.remove("active")
+        toggle.classList.add("inactive")
+      } else {
+        yearlyBilling = true
+        dot.style.float = "right"
+        dot.classList.add("active")
+        dot.classList.remove("inactive")
+      }
+})
 /*const allprices= [
     {
         pageviews: '10K',
@@ -28,7 +43,3 @@ let multiplier = yearlyBilling ? 0.75 : 1
 let finalPrice = allPrices[sv].price * multiplier
   htmlPrice.innerHTML = `$${finalPrice}.00`
   */
-
-range.addEventListener('input',function(){
-  return (range.textContent = this.value)
-})
