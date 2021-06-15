48cfb37a91d7b09b88e236ac53cb82c1b70574b4
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2021-06-15 19:06:39 -0300

    a

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+const allprices= [
+  {
+      pageviews: '10K',
+      price: 8
+  },
+  {
+      pageviews: '50K',
+      price: 12
+  },
+  {
+      pageviews: '100K',
+      price:16
+  },
+  {
+      pageviews: '500K',
+      price: 24
+  },
+  {
+      pageviews: '1M',
+      price: 36
+  }
+]
 let range  = document.getElementById('range')
 let pageviews = document.getElementById("pageviews")
 let price = document.getElementById("value")
@@ -15,31 +37,9 @@ biling.addEventListener("click",function(){
       biling.classList.remove("form-active")
     }
   })
-/*const allprices= [
-    {
-        pageviews: '10K',
-        price: 8
-    },
-    {
-        pageviews: '50K',
-        price: 12
-    },
-    {
-        pageviews: '100K',
-        price:16
-    },
-    {
-        pageviews: '500K',
-        price: 24
-    },
-    {
-        pageviews: '1M',
-        price: 36
-    }
-]
 pageviews.innerHTML = `${price.pageviews}`
 let multiplier = yearlyBilling ? 0.75 : 1
 let finalPrice = allPrices[sv].price * multiplier
   htmlPrice.innerHTML = `$${finalPrice}.00`
-  */
+
 console.log('OOLA')  
